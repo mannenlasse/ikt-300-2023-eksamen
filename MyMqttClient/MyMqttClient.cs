fed6687dadcf53a3ba59825ad7eced63659d2972
Ammar Ali <ammara@student.uia.no>
2023-12-04 21:13:43 +0100

    trying stuff
    truyng stuff

---
+++ MyMqttClient/MyMqttClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace MyMQTTClient
+{
+    public class MyMqttClient
+    {
+        
+
+        static MqttClient m_Client;
+        static string clientID;
+        static string m_PSUID;
+
+
+
+        public void Connect(object sender, EventArgs e)
+        {
+
+            string connectionString = "localhost";
+            m_Client = new MqttClient(connectionString);
+            clientID = Guid.NewGuid().ToString();
+            m_Client.Connect(clientID);
+        }
+        
+        
+
+        private void Publish(object sender, EventArgs e)
+        {
+            string topic = string.Format("/PSU/PSU2000/{0}/{1}", m_PSUID);
+            m_Client.Publish(topic, Encoding.UTF8.GetBytes(topic), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+        }
+        
+        
+    }
+    
+    
+    
+}
--- PsuController/Class1.cs
+++ PsuController/Class1.cs
@@ -1,11 +1,24 @@
-﻿using PsuController;
+﻿using System.Text;
+using PsuController;
+using MyMQTTClient;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using PsuManager;
+using uPLibrary.Networking.M2Mqtt;
+
 namespace PsuController
 {
+    
+
     public class Class1
     {
+        private IPsu _psu;
+        private MqttClient _mqttClient;
+       
+        
+        public void Publish()
+        {
 
-
-
-
+        }
     }
+    
 }
