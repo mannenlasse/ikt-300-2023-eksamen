bbea72b58ae0b54b040877d0708dba9d08be1b96
Ammar Ali <ammara@student.uia.no>
2023-12-05 14:12:13 +0100

    commented out mqqtclient fucntions

--- MyMqttClient/MyMqttClient.cs
+++ MyMqttClient/MyMqttClient.cs
@@ -13,27 +13,22 @@ namespace MyMQTTClient
         static string m_PSUID;
 
 
-
-        public void Connect(object sender, EventArgs e)
-        {
-
+       /*
+        public void Connect(object sender, EventArgs e) {
             string connectionString = "localhost";
             m_Client = new MqttClient(connectionString);
             clientID = Guid.NewGuid().ToString();
             m_Client.Connect(clientID);
         }
         
-        
-
-        private void Publish(object sender, EventArgs e)
-        {
+        private void Publish(object sender, EventArgs e) {
             string topic = string.Format("/PSU/PSU2000/{0}/{1}", m_PSUID);
             m_Client.Publish(topic, Encoding.UTF8.GetBytes(topic), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
         }
-        
-        
+       */
+
     }
-    
-    
-    
+
+
+
 }
