186231b16f541673a316eb3213c26fd33028ff02
Ammar Ali <ammara@student.uia.no>
2023-12-07 23:18:18 +0100

    merging usbcontroller and remote on off and operation branch together

--- MyMqttClient/MyMqttClient.cs
+++ MyMqttClient/MyMqttClient.cs
@@ -1,55 +1,12 @@
 ﻿using System.Text;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using uPLibrary.Networking.M2Mqtt;
-using PsuManager;
+
 namespace MyMQTTClient
 {
     public class MyMqtt
     {
-        private psuManager _psuManager = new psuManager();
-        private string clientID;
-        static string connectionstring = "localhost";
-        private string topic;
 
-        MqttClient client = new MqttClient(connectionstring);
-        
- 
-        public void connectClient()
-        {
-            
-            clientID = Guid.NewGuid().ToString();
-            
-            client.Connect(clientID);
-            Console.WriteLine("Connected to: " + clientID );
-            Console.WriteLine(" Connectionstring  is: " + connectionstring );
-            _psuManager.SerialNumber();
-
-        }
-        
-
-        
-        public void Subscribe(string topicInput)
-        {
-            
-            Console.WriteLine(topicInput);
-            string topic = string.Format("/PSU/PSU2000/{0}/#", topicInput);
-            client.Subscribe(new string[] { topic }, new byte[] { 2 });
-            Console.WriteLine("Subscribed to " + topic );
-            Console.WriteLine("Subscribed to " + topic );
-            
-        }
-
-        
-
-        public void publish(string topic, string message)
-        {
-     
-             topic = string.Format("/PSU/PSU2000/{0}/{1}", topic, message);
-            client.Publish(topic, Encoding.UTF8.GetBytes(topic), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
-            Console.WriteLine("Published message " + message + " to the topic: " + topic );
-        }
-        
-        
         
 
     }
--- UserInterface/Form1.cs
+++ UserInterface/Form1.cs
@@ -215,14 +215,13 @@ public partial class Form1 : Form
     private void button8_Click(object sender, EventArgs e)
     {
         //connect
-        _mqttClient.connectClient();
+        
     }
 
 
     private void button3_Click(object sender, EventArgs e)
     {
         //subscribe_button
-        _mqttClient.Subscribe(string.Format(textBox7.Text));
     }
 
 
@@ -230,7 +229,6 @@ public partial class Form1 : Form
     private void button7_Click(object sender, EventArgs e)
     {
         //publish_button
-        _mqttClient.publish(string.Format(textBox7.Text), string.Format(textBox6.Text));
 
     }
 
